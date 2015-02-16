3d2839e202993a69c9c687b3ec7747689cd67d96
Aldyaka Mushofan <mushofan@users.noreply.github.com>
2015-02-16 11:49:55 +0700

    Update validasi.js

--- js/validasi.js
+++ js/validasi.js
@@ -38,22 +38,3 @@ function Validation() {
 	
 	return true;
 }
-
-/*function resetForm(FormId){
-	document.forms[FormId].reset();
-}
-
-function makeYear(Tanggal){
-	var tahun = Tanggal.charAt(0) + Tanggal.charAt(1) + Tanggal.charAt(2) + Tanggal.charAt(3);
-	return parseInt(tahun);
-}
-
-function makeMonth(Tanggal){
-	var bulan = Tanggal.charAt(5) + Tanggal.charAt(6);
-	return parseInt(bulan);
-}
-
-function makeDay(Tanggal){
-	var hari = Tanggal.charAt(8) + Tanggal.charAt(9);
-	return parseInt(hari);
-}*/
